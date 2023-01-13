10548a84a4e2bf54467ba181a3ae96475db4d651
Meynard <meynard.trinidad44@gmail.com>
2023-01-13 15:45:03 +0800

    Added sidebar

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -4,17 +4,20 @@ import { Foreground, MidGround1, MidGround2, MountainBackground } from '../image
 const Home = () => {
   return (
     <div className='min-h-[100vh] w-full'>
-      <div className='absolute bottom-0'>
+      <div className='absolute bottom-0 w-full'>
         <MountainBackground />
       </div>
-      <div className='absolute bottom-28'>
+      <div className='absolute bottom-[12vh] w-full'>
         <MidGround2 />
       </div>
-      <div className='absolute bottom-0'>
+      <div className='absolute bottom-0 w-full'>
         <MidGround1 />
       </div>
-      <div className='absolute bottom-0'>
+      <div className='absolute bottom-0 w-full'>
         <Foreground />
+      </div>
+      <div className='w-[35%] h-full bg-slate-800/30 z-20 absolute right-0 flex justify-center items-center'>
+        <button className='p-4 text-white font-bold'>Change Time</button>
       </div>
     </div>
   )

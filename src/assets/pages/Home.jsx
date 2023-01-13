c83dc5887d8b058a9ce0b7552cbc4069c7f7bfd8
Miguel <miguelarciagaa@gmail.com>
2023-01-13 10:58:05 +0800

    Set up VITE TAILWIND CSS

---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-full w-full bg-red-400'>
+        <div className="min-h-full w-full bg-red-400">
+            hello?
+
+        </div>
+    </div>
+  )
+}
+
+export default Home

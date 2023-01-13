92babf79dfe86355e8046eea0aa2ab6b4bad2412
Meynard <meynard.trinidad44@gmail.com>
2023-01-13 14:27:05 +0800

    Added SVG background

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Foreground, MidGround1, MidGround2, MountainBackground } from '../images/Background';
 
 const Home = () => {
   return (
     <div className='min-h-[100vh] w-full'>
-      <img src="images/dawn_bg.svg" alt="dawn" />
-      
+      <div className='absolute bottom-0'>
+        <MountainBackground />
+      </div>
+      <div className='absolute bottom-28'>
+        <MidGround2 />
+      </div>
+      <div className='absolute bottom-0'>
+        <MidGround1 />
+      </div>
+      <div className='absolute bottom-0'>
+        <Foreground />
+      </div>
     </div>
   )
 }
